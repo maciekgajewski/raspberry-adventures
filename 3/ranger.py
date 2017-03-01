e40eab2114136624a8bcd8e3b284efa31d891bc5
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-01 19:28:31 +0000

    Linear ranger, much nicer, looks cool!

--- 3/ranger.py
+++ 3/ranger.py
@@ -35,13 +35,22 @@ def display(value):
 init()		
 runup()
 
-# Read sensor in a loop, display the value
-# Sensor retruns value from 0 (very close) to 515 (very far). It seems to be in cm
-max = math.log(515)
-while True:
-	reading = grovepi.ultrasonicRead(RANGER_SOCKET)
+def log_scale(reading):
+	max = math.log(515)
 	logrange = math.log(reading)
 	value = 1 - min(1.0, logrange/max)
+	return value
+
+def proximity_scale(reading, max_dist):
+	value = 1 - min(1.0, reading/max_dist)
+	return value
+
+
+# Read sensor in a loop, display the value
+# Sensor retruns value from 0 (very close) to 515 (very far). It seems to be in cm
+while True:
+	reading = grovepi.ultrasonicRead(RANGER_SOCKET)
+	value = proximity_scale(reading, 30)
 	display(value)
 	#print('reading={}, value={}'.format(reading, value))
 	time.sleep(0.1)
