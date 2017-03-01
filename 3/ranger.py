8e382d4c4b3907a3e47eafc06377131483a1496f
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-01 19:23:53 +0000

    logarythimc display works

---
+++ 3/ranger.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+import time
+import grovepi
+import math
+
+pins = [17, 18, 27, 22, 23, 24, 25, 4]
+RANGER_SOCKET = 4
+
+def init():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+
+	for pin in pins:
+		GPIO.setup(pin, GPIO.OUT)
+
+def runup():
+	for pin in pins:
+		GPIO.output(pin, GPIO.HIGH)
+		time.sleep(0.2)
+		GPIO.output(pin, GPIO.LOW)
+
+# Displays value [0, 1]
+def display(value):
+	leds = round(value * 8)
+	#print('usage={}, leds={}'.format(usage, leds))
+	for i in range(8):
+		pin = pins[i]
+		if i < leds:
+			GPIO.output(pin, GPIO.HIGH)
+		else:
+			GPIO.output(pin, GPIO.LOW)
+
+init()		
+runup()
+
+# Read sensor in a loop, display the value
+# Sensor retruns value from 0 (very close) to 515 (very far). It seems to be in cm
+max = math.log(515)
+while True:
+	reading = grovepi.ultrasonicRead(RANGER_SOCKET)
+	logrange = math.log(reading)
+	value = 1 - min(1.0, logrange/max)
+	display(value)
+	#print('reading={}, value={}'.format(reading, value))
+	time.sleep(0.1)
+		
+
+
+
