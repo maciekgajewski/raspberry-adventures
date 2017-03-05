b7d8d882d47b84a51aad0de7280a8d6337279443
Maciej Gajewski <maciej.gajewski0@gmail.com>
2017-03-05 18:19:43 +0100

    nice exchange of messages

--- 6/server.py
+++ 6/server.py
@@ -3,6 +3,7 @@
 import asyncio
 import websockets
 import time
+import json
 
 def forward():
 	print("moving forward...")
@@ -12,15 +13,35 @@ def backward():
 	print("moving backward...")
 	time.sleep(1)
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print("< {}".format(name))
+def process_command(cmd):
+	t = cmd['type']
+	if (t == 'command'):
+		c = cmd['command']
+		if c == 'forward':
+			forward()
+		elif c == 'backward':
+			backward()
+		else:
+			raise RuntimeError('unknown command')
+	else:
+		raise RuntimeError('unknown type')
 
-    greeting = "Hello {}!".format(name)
-    await websocket.send(greeting)
-    print("> {}".format(greeting))
+async def server_loop(websocket, path):
+	while True:
+		print('receiving...')
+		encoded = await websocket.recv()
+		decoded = json.loads(encoded)
+		print('>>>: {}'.format(encoded))	
+		reply = {'result' : 'OK' }
+		try:
+			process_command(decoded)
+		except Exception as e:
+			reply = {'result' : 'error', 'msg' : str(e)}
+			
+		encoded_reply = json.dumps(reply)
+		await websocket.send(encoded_reply)
 
-start_server = websockets.serve(hello, 'localhost', 1980)
+start_server = websockets.serve(server_loop, 'localhost', 1980)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
